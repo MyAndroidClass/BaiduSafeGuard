e702ded063085cb9c39316aea7301e06bdf963c6
hzl <1005672068@qq.com>
2017-06-15 14:43:21 +0800

    add myutils.java file

---
+++ dbphoneguard/src/main/java/com/zzptc/sky/dbphoneguard/utils/MyUtils.java
@@ -0,0 +1,12 @@
+package com.zzptc.sky.dbphoneguard.utils;
+
+/**
+ * Created by Administrator on 2017/6/15 015.
+ */
+
+public class MyUtils {
+
+    public static void println(String msg){
+        System.out.println(msg);
+    }
+}
